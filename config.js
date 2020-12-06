d422cbea40e745fd4b0f6518a2585e9cd412f6d5
fluffy <hgoziev001@gmail.com>
2020-12-06 21:44:11 +0500

    f"

--- config.js
+++
@@ -1,7 +0,0 @@
-const { model } = require("mongoose");
-
-module.exports = {
-  mongoDB:
-    "mongodb+srv://fluffy:fluffy09@cluster0.sghoa.mongodb.net/merng?retryWrites=true&w=majority",
-  SECRET_KEY: "super secret password",
-};
